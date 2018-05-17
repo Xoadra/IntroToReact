67cad28a1f25aa75680d428a2c4ffe9c2d5cd1fc
Xoadra <xijp77@gmail.com>
2018-05-17 16:17:49 -0700

    Tiny code cleanup of original starter files.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css'
 
 
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) )
+ReactDOM.render( <App/>, document.getElementById( 'root' ) )
 registerServiceWorker( )
 
 
