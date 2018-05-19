a18ddf3d6973232b79b98ccd2c84cf1ed22e8740
Xoadra <xijp77@gmail.com>
2018-05-19 09:49:06 -0700

    History of prior steps now traversable.

--- src/elements/Game.js
+++ src/elements/Game.js
@@ -20,6 +20,8 @@ class Game extends Component {
 			history: [ { squares: Array( 9 ).fill( null ) } ],
 			// Archive of the final state of all previous games played
 			games: [ ],
+			// Number of game steps taken within this series of play
+			stepNumber: 0,
 			// Track if player x goes next to see whose turn it is to go
 			xIsNext: true
 		}
@@ -29,7 +31,7 @@ class Game extends Component {
 	// Change squares from above the square's parent component
 	handleClick( insert ) {
 		// Edit state's history using a copy as this makes data safe
-		const history = this.state.history
+		const history = this.state.history.slice( 0, this.state.stepNumber + 1 )
 		const current = history[ history.length - 1 ]
 		// Replicate the latest squares array to track the turn taken
 		const squares = current.squares.slice( )
@@ -41,6 +43,8 @@ class Game extends Component {
 		this.setState( {
 			// Insert updated copy of squares array into history array
 			history: history.concat( [ { squares: squares } ] ),
+			// Document the quantity of steps throughout this game
+			stepNumber: history.length,
 			// Go to the next player's turn since last player just went
 			xIsNext: !this.state.xIsNext
 		} )
@@ -48,7 +52,7 @@ class Game extends Component {
 	
 	handleReset( winner ) {
 		// List state's arrays as immutable values because why not
-		const history = this.state.history
+		const history = this.state.history.slice( 0, this.state.stepNumber + 1 )
 		const games = this.state.games
 		// New winner property is added to see this game's winner
 		let replica = Object.assign( {  }, history[ history.length - 1 ] )
@@ -63,16 +67,35 @@ class Game extends Component {
 		this.setState( {
 			history: history.concat( [ { squares: Array( 9 ).fill( null ) } ] ),
 			games: games.concat( [ games ] ),
+			stepNumber: history.length,
 			xIsNext: true
+		} )
+	}
+	
+	jumpTo( step ) {
+		// Travel back to this step in history and wipe steps after it
+		this.setState( {
+			stepNumber: step,
+			xIsNext: ( step % 2 ) === 0
 		} )
 	}
 	
 	render( ) {
 		// Produce a copy of state's history for an unknown reason
 		const history = this.state.history
-		const current = history[ history.length - 1 ]
+		const current = history[ this.state.stepNumber ]
 		// Verify whether or not any player met a winning condition
 		const winner = calculateWinner( current.squares )
+		// Builds prior steps taken button for exploring game history 
+		const moves = history.map( ( step, move ) => {
+			const desc = move ? 'Move #' + move : 'Game Start'
+			return (
+				// Will need keys assigned as React uses keys for lists
+				<li key={ move }>
+					<button onClick={ ( ) => this.jumpTo( move ) }> { desc } </button>
+				</li>
+			)
+		} )
 		// Display game status according to if there is a winner yet
 		let status
 		if ( winner ) { status = 'Winner: ' + winner }
@@ -80,13 +103,13 @@ class Game extends Component {
 		else { status = 'Next player: ' + ( this.state.xIsNext ? 'X' : 'O' ) }
 		return (
 			<div className="game">
+				<div className="status"> { status } </div>
 				<div className="game-board">
 					<Board squares={ current.squares } onClick={ ( idx ) => this.handleClick( idx ) }/>
-					<Reset onClick={ ( ) => this.handleReset( winner ) }/>
 				</div>
+				<Reset onClick={ ( ) => this.handleReset( winner ) }/>
 				<div className="game-info">
-					<div className="status"> { status } </div>
-					<ol> {  } </ol>
+					<ol> { moves } </ol>
 				</div>
 			</div>
 		)
@@ -96,6 +119,5 @@ class Game extends Component {
 
 
 export default Game
-
 
 
