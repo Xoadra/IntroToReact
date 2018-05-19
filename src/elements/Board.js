24dc2fb3f1fb7d01ff1ffe6d38684ddb1c183c6f
Xoadra <xijp77@gmail.com>
2018-05-19 06:33:29 -0700

    Game history aggregation now in place.

--- src/elements/Board.js
+++ src/elements/Board.js
@@ -4,72 +4,26 @@
 
 import React, { Component } from 'react'
 import Square from './Square'
-import Reset from './Reset'
-import calculateWinner from '../actions/winner'
 
 
 
 class Board extends Component {
 	
-	constructor( props ) {
-		// Use props here by injecting inherited class props
-		super( props )
-		// Build an array for square values and fill with null
-		// Track if player x goes next to see which turn it is
-		this.state = { squares: Array( 9 ).fill( null ), xIsNext: true }
-	}
-	
-	
-	// Handle state on square's parent component instead
-	handleClick( insert ) {
-		// Copy state's squares array to track every update
-		const squares = this.state.squares.slice( )
-		// Exit early if someone won or if the square is filled
-		if ( calculateWinner( squares ) || squares[ insert ] ) { return }
-		// Insert current player's mark at this square's index
-		squares[ insert ] = this.state.xIsNext ? 'X' : 'O'
-		// Make changes to state's array using updated copy
-		// Go to next player's turn since last player just went
-		this.setState( { squares: squares, xIsNext: !this.state.xIsNext } )
-	}
-	
-	handleReset( winner ) {
-		// Generate a new property to see this game's winner
-		let replica = Object.assign( {  }, this.state )
-		replica.winner = winner
-		// Keep a record of each game like with saving turns
-		const game = Object.assign( {  }, replica )
-		// Objects with nested objects require deep copying
-		// Just use json methods for deep copies of an object
-		/* const game = JSON.parse( JSON.stringify( replica ) ) */
-		console.log( game )
-		// Reset the game's state which starts a new game
-		this.setState( { squares: Array( 9 ).fill( null ), xIsNext: true } )
-	}
-	
 	renderSquare( value ) {
 		return (
 			<Square
-				// Set square's value using parent state's array
-				value={ this.state.squares[ value ] }
-				// Pass the square a function to update values
-				onClick={ ( ) => this.handleClick( value ) }
+				// Set square's value via the passed down index
+				value={ this.props.squares[ value ] }
+				// Pass the square a function for updating values
+				onClick={ ( ) => this.props.onClick( value ) }
 			/>
 		)
 	}
 	
 	render( ) {
-		// Verify if a player has met a winning condition yet
-		const winner = calculateWinner( this.state.squares )
-		// Display game status by seeing if anyone has won
-		let status
-		if ( winner ) { status = 'Winner: ' + winner }
-		// Flip the status text to whichever player's turn it is
-		else { status = 'Next player: ' + ( this.state.xIsNext ? 'X' : 'O' ) }
 		return (
-			// Assign parent state's array index to each square
+			// Assign a numerical index values to every square
 			<div>
-				<div className="status"> { status } </div>
 				<div className="board-row">
 					{ this.renderSquare( 0 ) }
 					{ this.renderSquare( 1 ) }
@@ -85,7 +39,6 @@ class Board extends Component {
 					{ this.renderSquare( 7 ) }
 					{ this.renderSquare( 8 ) }
 				</div>
-				<Reset onClick={ ( ) => this.handleReset( winner ) }/>
 			</div>
 		)
 	}
@@ -94,6 +47,5 @@ class Board extends Component {
 
 
 export default Board
-
 
 
--- src/elements/Game.js
+++ src/elements/Game.js
@@ -4,17 +4,88 @@
 
 import React, { Component } from 'react'
 import Board from './Board'
+import Reset from './Reset'
+import calculateWinner from '../actions/winner'
 
 
 
 class Game extends Component {
 	
+	constructor( props ) {
+		// Use props here with the inherited component class' props
+		super( props )
+		this.state = {
+			// History of moves beginning with the initial game board
+			// Build an array for squares' values and set them to null
+			history: [ { squares: Array( 9 ).fill( null ) } ],
+			// Archive of the final state of all previous games played
+			games: [ ],
+			// Track if player x goes next to see whose turn it is to go
+			xIsNext: true
+		}
+	}
+	
+	
+	// Change squares from above the square's parent component
+	handleClick( insert ) {
+		// Edit state's history using a copy as this makes data safe
+		const history = this.state.history
+		const current = history[ history.length - 1 ]
+		// Replicate the latest squares array to track the turn taken
+		const squares = current.squares.slice( )
+		// Quit early if either player has won or if the square is filled
+		if ( calculateWinner( squares ) || squares[ insert ] ) { return }
+		// Write the current player's symbol into this square's index
+		squares[ insert ] = this.state.xIsNext ? 'X' : 'O'
+		// Make the updates to state to reflect this point in the game
+		this.setState( {
+			// Insert updated copy of squares array into history array
+			history: history.concat( [ { squares: squares } ] ),
+			// Go to the next player's turn since last player just went
+			xIsNext: !this.state.xIsNext
+		} )
+	}
+	
+	handleReset( winner ) {
+		// List state's arrays as immutable values because why not
+		const history = this.state.history
+		const games = this.state.games
+		// New winner property is added to see this game's winner
+		let replica = Object.assign( {  }, history[ history.length - 1 ] )
+		replica.winner = winner
+		// Keep a record of the game similar to how turns are saved
+		const game = Object.assign( {  }, replica )
+		// Objects with any nested objects will require deep copying
+		// Just use json methods to create deep copies of an object
+		/* const game = JSON.parse( JSON.stringify( replica ) ) */
+		console.log( game )
+		// Save this game's results and prepare a new game board
+		this.setState( {
+			history: history.concat( [ { squares: Array( 9 ).fill( null ) } ] ),
+			games: games.concat( [ games ] ),
+			xIsNext: true
+		} )
+	}
+	
 	render( ) {
+		// Produce a copy of state's history for an unknown reason
+		const history = this.state.history
+		const current = history[ history.length - 1 ]
+		// Verify whether or not any player met a winning condition
+		const winner = calculateWinner( current.squares )
+		// Display game status according to if there is a winner yet
+		let status
+		if ( winner ) { status = 'Winner: ' + winner }
+		// Flip the status text to show whichever player's turn it is
+		else { status = 'Next player: ' + ( this.state.xIsNext ? 'X' : 'O' ) }
 		return (
 			<div className="game">
-				<div className="game-board"><Board/></div>
+				<div className="game-board">
+					<Board squares={ current.squares } onClick={ ( idx ) => this.handleClick( idx ) }/>
+					<Reset onClick={ ( ) => this.handleReset( winner ) }/>
+				</div>
 				<div className="game-info">
-					<div> {  } </div>
+					<div className="status"> { status } </div>
 					<ol> {  } </ol>
 				</div>
 			</div>
@@ -25,5 +96,6 @@ class Game extends Component {
 
 
 export default Game
+
 
 
